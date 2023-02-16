741e1a2cce2ad96a56831b596604023b8c320c91
judigot <judigot@github.com>
2023-02-17 00:37:53 +0800

    Add test script to query database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,32 @@ import styles from "./page.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
-  return <main className={styles.main}></main>;
+import { PrismaClient } from "@prisma/client";
+
+import crypto from "crypto";
+
+const prisma = new PrismaClient();
+
+const Users = prisma.app_user;
+
+const tableName = "app_user";
+
+const getUsers = async () => {
+  try {
+    const result = prisma.$queryRawUnsafe(`SELECT * FROM ${tableName};`);
+    return result;
+  } catch (error) {
+    return error;
+  }
+};
+
+export default async function Home() {
+  const users: any = await getUsers();
+  return (
+    <main className={styles.main}>
+      {users?.map((row: { [key: string]: string }, i: number) => {
+        return <p key={i}>{row?.username}</p>;
+      })}
+    </main>
+  );
 }
