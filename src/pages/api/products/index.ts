e9d12907a31d0ccd5216c401ecd4355041d7a556
judigot <judigot@github.com>
2023-02-24 00:53:39 +0800

    Refactor to use DatatypeParser

--- src/pages/api/products/index.ts
+++ src/pages/api/products/index.ts
@@ -1,21 +1,24 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
-import { PrismaClient } from "@prisma/client";
 import { Decimal } from "@prisma/client/runtime";
+
+import DatatypeParser from "@utilities/DatatypeParser";
+
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 const Product = prisma.product;
 
 type Data = {
-  // product_id: number;
-  // product_name: string;
-  // product_category: number;
-  // product_cost: Decimal;
-  // product_price: Decimal;
-  // product_stock: BigInt;
-  // status: string;
+  product_id: number;
+  product_name: string;
+  product_category: number;
+  product_cost: Decimal;
+  product_price: Decimal;
+  product_stock: BigInt;
+  status: string;
 };
 
 export default function handler(
@@ -23,77 +26,11 @@ export default function handler(
   res: NextApiResponse<Data>
 ) {
   const { product_id } = req.query;
-  // Product.findUnique({
-  //   where: {
-  //     product_id: BigInt(product_id as string),
-  //   },
-  // })
-    prisma
-      .$queryRawUnsafe(
-        `SELECT * FROM "product";`
-        // `SELECT * FROM "product" WHERE "product_id" = ${product_id};`
-      )
+  Product.findMany()
     .then((result: any) => {
       // Success
-      let cleanedResult = undefined;
-
-      let cleanRow: { [key: string]: string | number | Decimal } | undefined =
-        {};
-
-      if (Array.isArray(result)) {
-        cleanRow = undefined;
-        cleanedResult = result.map(
-          (result: { [key: string]: string | number }, i: number) => {
-            const cleanRow: { [key: string]: string | number } = {};
-            // prettier-ignore
-            for ( let i = 0, arrayLength = Object.keys(result).length; i < arrayLength; i++ ) {
-              const key: string = Object.keys(result)[i];
-              const value = result[key];
-              switch (typeof value) {
-                case "bigint":
-                  cleanRow[key] = parseInt((value as number).toString());
-                  break;
-                case "object":
-                  cleanRow[key] = parseFloat(value);
-                  break;
-                default:
-                  cleanRow[key] = value;
-                  break;
-              }
-            }
-            return cleanRow;
-          }
-        );
-      } else {
-        // prettier-ignore
-        for ( let i = 0, arrayLength = Object.keys(result).length; i < arrayLength; i++ ) {
-          const key: string = Object.keys(result)[i];
-          const value = result[key];
-          switch (typeof value) {
-            case "bigint":
-              cleanRow[key] = parseInt((value as unknown as number).toString());
-              break;
-            case "object":
-              cleanRow[key] = parseFloat(value);
-              break;
-            default:
-              cleanRow[key] = value;
-              break;
-          }
-        }
-      }
-
-      res.status(200).json(cleanRow || (cleanedResult as Data));
-
-      // {
-      //   product_id: 6,
-      //   product_name: "Sandwich Bread",
-      //   product_category: 2,
-      //   product_cost: 19 as unknown as Decimal,
-      //   product_price: 29.99 as unknown as Decimal,
-      //   product_stock: BigInt(0),
-      //   status: "active",
-      // }
+      const data = DatatypeParser(result) as unknown as Data;
+      res.status(200).json(data);
     })
     .catch((error: string) => {
       // Failure
