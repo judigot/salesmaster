a51cdb70b25b76798a549ed71257bfb958ea84ba
judigot <judigot@github.com>
2023-02-24 00:35:18 +0800

    Add utility function to parse postgres datatypes

---
+++ src/utilities/DatatypeParser.ts
@@ -0,0 +1,47 @@
+import { Decimal } from "@prisma/client/runtime";
+
+export default function castRows(result: any) {
+  // Success
+  let cleanedResult = undefined;
+  let cleanRow: { [key: string]: string | number | Decimal } | undefined = {};
+
+  // If multiple rows
+  if (Array.isArray(result)) {
+    cleanRow = undefined;
+    cleanedResult = result.map(
+      (result: { [key: string]: string | number }, i: number) => {
+        return castRowValues(result);
+      }
+    );
+    return cleanedResult;
+  }
+
+  // If only one row
+  if (!Array.isArray(result)) {
+    cleanRow = castRowValues(result);
+    return cleanRow;
+  }
+}
+
+const castRowValues = (data: any) => {
+  if (data) {
+    let rows: { [key: string]: string | number | Decimal } | undefined = {};
+    // prettier-ignore
+    for ( let i = 0, arrayLength = Object.keys(data).length; i < arrayLength; i++ ) {
+    const key: string = Object.keys(data)[i];
+    const value = data[key];
+    switch (typeof value) {
+      case "bigint":
+        rows[key] = parseInt((value as unknown as number).toString());
+        break;
+      case "object":
+        rows[key] = parseFloat(value);
+        break;
+      default:
+        rows[key] = value;
+        break;
+    }
+  }
+    return rows;
+  }
+};
