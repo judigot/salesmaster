54a4b4c22c6d538e8775e93e636e27608b9aff53
judigot <judigot@github.com>
2023-02-23 21:46:56 +0800

    Update middleware boilerplate

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,8 @@
-// middleware.ts
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-// Limit the middleware to paths starting with `/api/`
-export const config = {
-  matcher: "/api/:resource*",
-};
+export function AuthMiddleware(req: NextRequest, res: NextResponse) {}
 
-export function middleware(request: NextRequest, res: NextResponse) {
-  const isAuth = true;
-  if (isAuth) {
-    return new NextResponse(
-      JSON.stringify({ success: true, message: "Authentication successful" }),
-      { status: 200, headers: { "content-type": "application/json" } }
-    );
-  } else {
-    return new NextResponse(
-      JSON.stringify({ success: true, message: "Authentication successful" }),
-      { status: 200, headers: { "content-type": "application/json" } }
-    );
-  }
+export default function middleware(req: NextRequest, res: NextResponse) {
+  return AuthMiddleware(req, res);
 }
