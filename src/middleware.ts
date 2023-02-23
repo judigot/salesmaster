f6587aac13799c417e23044771954a4c307c941e
judigot <judigot@github.com>
2023-02-24 02:38:42 +0800

    Initialize variables for future use

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,5 +4,8 @@ import type { NextRequest } from "next/server";
 export function AuthMiddleware(req: NextRequest, res: NextResponse) {}
 
 export default function middleware(req: NextRequest, res: NextResponse) {
+  const authHeader = req.headers.get("authorization");
+  const accessToken = authHeader && authHeader.split(" ")[1];
+  const secretToken = process.env.ACCESS_TOKEN_SECRET;
   return AuthMiddleware(req, res);
 }
