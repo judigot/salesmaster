d41bd576c4caf69bb81aee0b0c0c8e38b5d0a8c0
judigot <judigot@github.com>
2023-03-12 14:21:56 +0800

    Add recursion for objects

--- src/utilities/DatatypeParser.ts
+++ src/utilities/DatatypeParser.ts
@@ -3,7 +3,9 @@ import { Decimal } from "@prisma/client/runtime";
 export default function castRows(result: any) {
   // Success
   let cleanedResult = undefined;
-  let cleanRow: { [key: string]: string | number | Decimal } | undefined = {};
+  let cleanRow:
+    | { [key: string]: string | number | Decimal | Object }
+    | undefined = {};
 
   // If multiple rows
   if (Array.isArray(result)) {
@@ -26,22 +28,31 @@ export default function castRows(result: any) {
 const castRowValues = (data: any) => {
   if (data) {
     let rows: { [key: string]: string | number | Decimal } | undefined = {};
-    // prettier-ignore
-    for ( let i = 0, arrayLength = Object.keys(data).length; i < arrayLength; i++ ) {
-    const key: string = Object.keys(data)[i];
-    const value = data[key];
-    switch (typeof value) {
-      case "bigint":
-        rows[key] = parseInt((value as unknown as number).toString());
-        break;
-      case "object":
-        rows[key] = parseFloat(value);
-        break;
-      default:
-        rows[key] = value;
-        break;
+
+    for (
+      let i = 0, arrayLength = Object.keys(data).length;
+      i < arrayLength;
+      i++
+    ) {
+      const key: string = Object.keys(data)[i];
+      const value = data[key];
+      switch (typeof value) {
+        case "bigint":
+          rows[key] = parseInt((value as unknown as number).toString());
+          break;
+        case "object":
+          if (Object.keys(value).length) {
+            // Recurse if row is an object
+            rows[key] = castRows(value) as unknown as any;
+          } else {
+            rows[key] = parseFloat(value);
+          }
+          break;
+        default:
+          rows[key] = value;
+          break;
+      }
     }
-  }
     return rows;
   }
 };
