219500b9bf772a5d3b97a3b3cc3a6f77dae3e760
Jude Francis Igot <34589286+judigot@users.noreply.github.com>
2024-01-16 15:52:20 +0800

    Update DatatypeParser.ts

--- src/utilities/DatatypeParser.ts
+++ src/utilities/DatatypeParser.ts
@@ -1,74 +1,61 @@
-import { Decimal } from "@prisma/client/runtime/library";
+import {Decimal} from '@prisma/client/runtime/library';
 
-export default function castRows(result: any) {
+// Define a type for row data
+type RowData = {
+  [key: string]: string | number | Decimal | Date | Object | RowData[];
+};
+
+export default function castRows<T extends RowData | RowData[]>(result: T): T {
   // Success
-  let cleanedResult = undefined;
-  let cleanRow:
-    | { [key: string]: string | number | Decimal | Object }
-    | undefined = {};
+  let cleanedResult: T;
 
   // If multiple rows
   if (Array.isArray(result)) {
-    cleanRow = undefined;
-    cleanedResult = result.map(
-      (result: { [key: string]: string | number }, i: number) => {
-        return castRowValues(result);
-      }
-    );
+    cleanedResult = result.map((row: RowData) => castRowValues(row)) as T;
     return cleanedResult;
   }
 
   // If only one row
   if (!Array.isArray(result)) {
-    cleanRow = castRowValues(result);
-    return cleanRow;
+    cleanedResult = castRowValues(result as RowData) as T;
+    return cleanedResult;
   }
+
+  throw new Error('Invalid input'); // or return a default value as needed
 }
 
-const castRowValues = (data: any) => {
-  if (data) {
-    let rows:
-      | { [key: string]: string | number | Decimal | Date | Object }
-      | undefined = {};
+const castRowValues = (data: RowData): RowData => {
+  let rows: RowData = {};
 
-    for (
-      let i = 0, arrayLength = Object.keys(data).length;
-      i < arrayLength;
-      i++
-    ) {
-      const key: string = Object.keys(data)[i];
-      const value = data[key];
-      switch (typeof value) {
-        case "bigint":
-          rows[key] = parseInt((value as unknown as number).toString());
-          break;
-        case "object":
-          if (value instanceof Decimal) {
-            rows[key] = parseFloat(value as unknown as string);
-          }
+  for (const [key, value] of Object.entries(data)) {
+    switch (typeof value) {
+      case 'bigint':
+        rows[key] = Number(value.toString());
+        break;
+      case 'object':
+        if (value instanceof Decimal) {
+          rows[key] = parseFloat(value as unknown as string);
+        }
 
-          // Array
-          if (
-            value instanceof Array &&
-            Array.isArray(value) &&
-            value.constructor.name === "Array"
-          ) {
-            // Recurse if row is an array
-            rows[key] = castRows(value) as unknown as Object;
-          }
+        // Array
+        if (
+          value instanceof Array &&
+          Array.isArray(value) &&
+          value.constructor.name === 'Array'
+        ) {
+          // Recurse if row is an array
+          rows[key] = castRows(value) as unknown as Object;
+        }
 
-          // Date/Datetime
-          if (value instanceof Date) {
-            rows[key] = new Date(value);
-          }
-          break;
-
-        // String
-        default:
-          rows[key] = value;
-          break;
-      }
+        // Date/Datetime
+        if (value instanceof Date) {
+          rows[key] = new Date(value);
+        }
+        break;
+      default:
+        rows[key] = value;
+        break;
     }
-    return rows;
   }
+  return rows;
 };
